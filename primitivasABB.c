3a4b173ce4e20e7a906422d9445c33370e9cc313
Leonel R <leorhcp2030@gmail.com>
2018-05-21 18:54:36 -0300

    Agrego abb_guardar
    marto no esta terminada la primitiva sino es una idea baga. segun de lo q estube mirando en la pagina donde podes constatar los algoritmos

--- primitivasABB.c
+++ primitivasABB.c
@@ -1,6 +1,9 @@
 #gratis
 #leo
 
+#define RAMA_DERECHA 1
+#define RAMA_IZQUIERDA -1
+
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
   abb_t* abb = malloc (sizeof(abb_t));
 
@@ -15,4 +18,52 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
   abb->raiz = NULL; 
 
   return abb;
+}
+
+size_t abb_cantidad(abb_t *arbol){
+  return arbol->cantidad;
+}
+
+
+bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
+  //tenemos que comparar las claves
+  // lo vi en la pagina de los algoritmos
+
+  int rama = arbol->comparar ( clave , arbol->clave );
+
+  if ( rama == RAMA_DERECHA ){
+
+    if( arbol->der ){
+    }
+    else{
+      nodo_t* nodo = nodo_crear(clave, dato);
+
+      if(!nodo) return false;
+
+      arbol->der = nodo;
+      arbol->cantidad++;
+      return true;
+    }
+  }
+
+  if ( rama == RAMA_IZQUIERDA ){
+    if( arbol->der ){
+    }
+    else{
+      nodo_t* nodo = nodo_crear(clave, dato);
+
+      if(!nodo) return false;
+
+      arbol->der = nodo;
+      arbol->cantidad++;
+      return true;
+    }
+  }
+
+  /* si caigo aca es por las claves son identicas
+
+      ... hacer esto 
+  
+ */
+  
 }
